c3ac055d40ec748a798d442ebd3131c681f922ac
PBaloyi449 <pbaloyi449@gmail.com>
2024-02-27 14:10:27 +0200

    Getting the nav toggle to be functional

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-document.getElementById("mobile-menu-toggle")
-        .addEventListener("click", function(){
-            document.getElementById("mobile-menu").classList.toggle("hidden")
-})
+ // Get the navigation toggle and menu elements
+//document.getElementById("mobile-menu-toggle")
+  //      .addEventListener("click", function(){
+ //           document.getElementById("mobile-menu").classList.toggle("hidden")
+//})
+
+
